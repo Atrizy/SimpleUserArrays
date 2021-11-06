606ef5ca2af34702f8b81a96e6e98716f0ea052f
Atrizy <evankeeping21@gmail.com>
2021-11-06 20:09:51 -0230

    added the bonus

--- app.js
+++ app.js
@@ -7,3 +7,28 @@ for(var i = 0; i < tweets.length; i++){
         console.log(`Something went wrong!`);
     }
 }
+
+var tweet = [
+    {
+        content: "Tweets are so cool",
+        posted_at: "2021-11-06",
+        username: "awsomeUser",
+        age: "17"
+    },
+    
+    {
+        content: "Tweets are not cool",
+        posted_at: "2020-11-06",
+        username: "notawsomeUser",
+        age: "27"
+    },
+    
+    {
+        content: "Tweets are so uncool",
+        posted_at: "2019-11-06",
+        username: "coolUser",
+        age: "19"
+    },
+    ];
+    
+    console.log(tweet)
